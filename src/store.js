3a509ae27c2640fca26b266d3339d44e54727c0e
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-25 11:37:54 -0500

    Add logger to log store status

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import missionsReducer from './redux/missions';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
   },
+  middleware: [logger],
 });
 
 export default store;
