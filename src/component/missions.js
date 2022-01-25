b49e15823ed5008b414837c5d045ca4e0ed118cd
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-01-25 15:32:48 -0500

    Add table to display missions

--- src/component/missions.js
+++ src/component/missions.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { Button, Table } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchMissions from '../fetchData';
 import { loadMissions } from '../redux/missions/missions';
@@ -15,7 +16,33 @@ const Missions = () => {
   }, []);
   return (
     <div>
-      Fetching missions
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Missions</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Join</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_name}</td>
+              <td>
+                {mission.description}
+                {' '}
+              </td>
+              <td>
+                <Button>Not a member</Button>
+                {' '}
+              </td>
+              <td><Button>Join mission</Button></td>
+
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
