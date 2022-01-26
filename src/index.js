3f83cf5db8a5e0ec0e83c0789ebcfc9eee8e6c67
rtonata88 <rtonata@gmail.com>
2022-01-26 19:00:01 +0200

    Removed vitals

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Rockets from './component/RocketsContainer';
 import Missions from './component/Missions';
 import MyProfile from './component/Profile';
@@ -24,10 +23,5 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
