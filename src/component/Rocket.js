64dc37396e2bc1a624e34f4ae0da7de6f7f78adf
rtonata88 <rtonata@gmail.com>
2022-01-26 20:42:29 +0200

    Display reserved badge for reserved rockets

--- src/component/Rocket.js
+++ src/component/Rocket.js
@@ -4,22 +4,30 @@ import PropTypes from 'prop-types';
 const Rocket = ({ rocket, reserveRocket, cancelReservation }) => {
   const { rocketName, rocketType, flickrImages } = rocket;
   let button;
+  let badge;
   if (rocket.reserved) {
     button = (
       <button type="button" onClick={cancelReservation}>
         Cancel Reservation
       </button>
     );
+
+    badge = 'Reserved - ';
   } else {
     button = (
       <button type="button" onClick={reserveRocket}>
         Reserve Rocket
       </button>
     );
+
+    badge = '';
   }
   return (
     <div>
-      <p>{rocketName}</p>
+      <p>
+        {badge}
+        {rocketName}
+      </p>
       <p>{rocketType}</p>
       {button}
       <p>
